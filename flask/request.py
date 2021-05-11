d4073e11b8d6652ef99c2d5f98cb79ef4fb8386d
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-05-12 01:01:02 +0900

    finish get/post/put/delete

---
+++ flask/request.py
@@ -0,0 +1,21 @@
+import requests
+
+r = requests.get(
+  'http://127.0.0.1:5000/employee/jun'
+)
+print(r.text)
+r = requests.post(
+  'http://127.0.0.1:5000/employee', data={'username': 'jun'}
+)
+print(r.text)
+r = requests.put(
+  'http://127.0.0.1:5000/employee', data={
+    'username': 'jun',
+    'new_name': 'sakai'
+    }
+)
+print(r.text)
+r = requests.delete(
+  'http://127.0.0.1:5000/employee', data={'username': 'jun'}
+)
+print(r.text)
